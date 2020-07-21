f7e897b9cfb6aa3db22ff2e04461febd45a75ef0
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2020-07-21 13:35:59 +0200

    Connects to server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 import productRoutes from "./routes/productRoutes";
 
@@ -8,4 +9,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(productRoutes);
 
-app.listen(4000);
+mongoose
+  .connect(process.env.CONNECTION_STRING)
+  .then((result) => {
+    app.listen(4000);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
